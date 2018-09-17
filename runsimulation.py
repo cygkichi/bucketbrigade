0982fe56320ee6f5aaa25a8827ecd4205fd02f29
hoge <hoge>
2018-09-18 00:25:31 +0900

    09/17

--- runsimulation.py
+++ runsimulation.py
@@ -2,76 +2,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 from graphviz import Digraph
 
-def branch_cut(w, cut_matrixs):
-    b, N, _ = cut_matrixs.shape
-    wc = np.copy(w)
-    for cut_mat in cut_matrixs:
-        wc = wc * cut_mat
-    return wc
+sample_N=11
+sample_M=2
+sample_edges = np.array([[0,1],[2,3],[1,4],[3,4],\
+                         [4,5],[5,6],[6,7],[6,9],\
+                         [7,8],[9,10],[8,0],[10,2]],dtype=int)
+sample_state = np.zeros([sample_N, sample_M], dtype=int)
+sample_state[5] = [1, 0]
+sample_state[9] = [0, 1]
+w0 = np.array([[0,0,0,0],[0,0,0,0],[1,1,0,0],[0,0,1,0]])
+w1 = np.array([[0,1,0,0],[0,0,0,0],[1,0,0,0],[1,0,0,0]])
 
-def anti_collision(wc, now_state):
-    N, _ = wc.shape
-    isfilled = np.sum(now_state, axis=1) > 0
-    isstop = np.sum(wc[isfilled], axis=0) > 0
-    ws = np.copy(wc)
-    ws[:, isstop] = np.eye(N, dtype=np.int)[:, isstop]
-    return ws
+def gen_cuttedroutes(route):
+    cutted_routes = None
+    return cutted_routes
+
+def block(route, state):
+    """ OK
+    : (in)route ... is NxN matrix.
+    : (in)state ... is NxM matrix.
+    : (out)croute ... is NxN matrix. cuted branch.
+    """
+    N, _     = net.shape
+    isfilled = np.sum(route, axis=1) > 0
+    isstop   = np.sum(route[isfilled], axis=0) > 0
+    croute   = np.copy(route)
+    croute[:, isstop] = np.eye(N, dtype=np.int)[:, isstop]
+    return croute
 
 def entry_box(now_state, next_state, p_entry):
     return None
 
 def calc_reward(next_state, reward_vec):
-    return None
-
+    reward = None
+    terminal = True
+    return reward, terminal
 
 class Simulator:
     def setup_sample_network(self):
-        N = 11; M = 2
+        N = sample_N; M = sample_M
         self.n_node = N
         self.n_spec = M
-        self.edges = np.array([[0,1],[2,3],[1,4],[3,4],[4,5],\
-                               [5,6],[6,7],[6,9],[7,8],[9,10],[8,0],[10,2]],dtype=int)
-        self.w = np.zeros([N, N], dtype=int)
-        self.w[self.edges[:,1], self.edges[:,0]] = 1
-        self.p_entry = None
+        self.edges  = sample_edges
+        self.route  = np.zeros([N, N], dtype=int)
+        self.route[self.edges[:,1], self.edges[:,0]] = 1
+        self.p_entry    = None
         self.reward_vec = None
-        self.state = np.zeros([N,M], dtype=int)
-        self.state[5] = [1,0]
-        self.state[9] = [0,1]
-        cut_matrixs =[]
-        for i, hline in enumerate(self.w):
-            if np.sum(hline) > 1:
-                mats = []
-                for j,v in enumerate(hline):
-                    if v > 0:
-                        mat = np.ones([N,N], dtype=int)
-                        mat[i] = 0
-                        mat[i,j] = 1
-                        mats.append(mat)
-                cut_matrixs.append(mats)
-        for j, vline in enumerate(self.w.T):
-            if np.sum(vline) > 1:
-                mats = []
-                for i,v in enumerate(vline):
-                    if v > 0:
-                        mat = np.ones([N,N], dtype=int)
-                        mat[:,j] = 0
-                        mat[i,j] = 1
-                        mats.append(mat)
-                cut_matrixs.append(mats)
-        self.cut_matrix_set = np.array(cut_matrixs, dtype=np.int)
+        self.state      = sample_state
+        self.cutted_routes = gen_cuttedroutes(self.route)
 
-    def next(self, action):
+    def step(self, action):
         now_state = self.state
-        cut_matrixs = self.cut_matrix_set[[0,1],action]
-        self.wc = branch_cut(self.w, cut_matrixs)
-        self.ws = anti_collision(self.wc, now_state)
-        next_state = np.dot(self.ws, now_state)
-        #next_state = entry_box(now_state, next_state, self.p_entry)
-        #reward = calc_reward(next_state, self.reward_vec)
+        croute = self.cutted_routes[action]
+        croute = block(croute, now_state)
+        next_state = np.dot(croute, now_state)
+        next_state = entry_box(now_state, next_state, self.p_entry)
+        reward, terminal = calc_reward(next_state, self.reward_vec)
         self.state = next_state
-        terminal = True
-#        return self.state, terminal, reward
+        return self.state, terminal, reward
 
     def make_graph(self, output='graph'):
         G = Digraph(format = 'png')
@@ -83,4 +71,4 @@ if __name__ == '__main__':
     env = Simulator()
     env.setup_sample_network()
     env.make_graph()
-    env.next([0,1])
+    env.step([0,1])
